4571cd20def2044573c3ba4183df51d2b9a22544
Geoff Vincent <geoffkvincent@gmail.com>
2018-09-10 16:11:29 -0600

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'boards#index'
+
+  resources :boards do
+    resources :lists
+  end
+
+  scope 'lists/:list_id', as: 'list' do
+    resources :tasks
+  end
+  
 end
