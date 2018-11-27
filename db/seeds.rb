928a1f3e9f617c9a41d5ac804e6436a0b22fe63f
Geoff Vincent <geoffkvincent@gmail.com>
2018-11-27 15:10:39 -0700

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,28 @@
-User.create(email: "test@test.com", password: "password", password_confirmation: "password")
-10.times do 
-  Boards.create(
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
-    email: Faker::Internet.email,
-    phone: Faker::PhoneNumber.phone_number,
-    user_id: 1
+1.times do
+  u = User.create(
+  email: 'admin@admin.com',
+  password: 'password',
+  nickname: 'coolguy'
   )
-  5.times do 
-    
+  10.times do
+    l = u.lists.create(
+    name: Faker::Hipster.word,
+    description: Faker::Hipster.sentence(4),
+    location: Faker::Address.street_address,
+    priority: Faker::Number.between(1,10),
+    active: Faker::Boolean.boolean(0.7)
+    )
+    10.times do
+      l.items.create(
+        name: Faker::Commerce.product_name,
+        description: Faker::Hipster.sentence(2),
+        priority: Faker::Number.between(1,100),
+        active: Faker::Boolean.boolean(0.4),
+        comment: Faker::Hipster.sentence(1),
+        list_id: l.id
+      )
+    end
+  end
 end
+
+puts "seeded"
